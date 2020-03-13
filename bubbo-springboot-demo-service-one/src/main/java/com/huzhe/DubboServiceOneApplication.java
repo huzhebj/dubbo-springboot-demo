64e7c85897b9180d9b2d35d21520643cd2b02acd
胡哲 <huzhe@xiaoshouyi.com>
2020-03-13 13:31:03 +0800

    commit,0313,13:30”

--- bubbo-springboot-demo-service-one/src/main/java/com/huzhe/DubboServiceOneApplication.java
+++ bubbo-springboot-demo-service-one/src/main/java/com/huzhe/DubboServiceOneApplication.java
@@ -1,13 +1,16 @@
 package com.huzhe;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableDubbo
 @SpringBootApplication
 public class DubboServiceOneApplication {
-
+    private static final Logger log = LoggerFactory.getLogger(DubboServiceOneApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(DubboServiceOneApplication.class, args);
+        log.info("dubbo-springboot-demo-service-one started ... ...");
     }
 }
